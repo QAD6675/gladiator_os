d76b22e65044bbef7868bcb5c5932bb8041e060c
qad7098 <qadirixx01@gmail.com>
2024-02-04 19:35:57 +0100

    basic writing functionality

--- src/kernel/arch/i686/keyboard.c
+++ src/kernel/arch/i686/keyboard.c
@@ -1,5 +1,6 @@
 #include "keyboard.h"
 #include <stdint.h>
+#include "irq.h"
 #include "io.h"
 #include "isr.h"
 #include <stdio.h>
@@ -9,40 +10,22 @@
 #define NORMAL_STATE 0
 #define PREFIX_STATE 1
 
-typedef struct {
-    uint8_t code;
-} key_event;
-
-key_event keyboard_buffer[MAX_KEYB_BUFFER_SIZE];
+int keyboard_buffer[MAX_KEYB_BUFFER_SIZE];
 uint8_t buf_position = 0;
-uint8_t current_state;
+uint8_t current_state=NORMAL_STATE;
+int prefix='\0'
 
 void init_keyboard() {
     i686_IRQ_RegisterHandler(1, getKey);
     current_state = NORMAL_STATE;
 }
 
-int getKey(Registers* regs)
+void getKey(Registers* regs)
 {
     int scancode = i686_inb(0x60); // Read byte from the Keyboard data port
 
-    keyboard_buffer[buf_position].code = scancode;
+    keyboard_buffer[buf_position]= scancode;
     buf_position = (buf_position + 1) % MAX_KEYB_BUFFER_SIZE;
-
-    printf("%i\n",scancode);
-    return scancode;
-}
-
-
-void keyboard_driver_irq_handler() {
-    int scancode = i686_inb(0x60); // Read byte from the Keyboard data port
-    if (scancode == 0xE0) {
-        current_state = PREFIX_STATE;
-        // We have read a prefix, so update the state and exit.
-        return ;
-    }
-    if (current_state == PREFIX_STATE) {
-        // Store the next part of the scancode, then return to normal state.
-        current_state = NORMAL_STATE;
-    }
+    printf("%c",chars[scancode]);
+    return ;
 }
