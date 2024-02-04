21dfe59d0e32474154c620cbe5490b7fc2bb3f2d
qad7098 <qadirixx01@gmail.com>
2024-02-04 22:59:54 +0100

    fully functional keyboard driver (buggy caps lock)

--- src/kernel/arch/i686/keyboard.c
+++ src/kernel/arch/i686/keyboard.c
@@ -19,6 +19,7 @@ typedef struct {
         bool super;
         bool caps;
         bool none;
+        int last;
 }Kstate;
 
 Kstate state={
@@ -28,58 +29,78 @@ Kstate state={
     .super=false,
     .caps=false,
     .none=true,
+    .last='\0'
 };
+
+void keyboard_irq(Registers* regs)
+{
+    int scancode = i686_inb(0x60); // Read byte from the Keyboard data port
+    int c=chars[scancode];
+
+    switch(scancode){//disableing mods on key up
+        case 219:
+            state.super=false;
+            if(state.shift==false &&state.alt==false && state.ctrl==false && state.super==false && state.caps==false){state.none=true;}
+            return;
+        case 184:
+            state.alt=false;
+            if(state.shift==false &&state.alt==false && state.ctrl==false && state.super==false && state.caps==false){state.none=true;}
+            return;
+        case 157:
+            state.ctrl=false;
+            if(state.shift==false &&state.alt==false && state.ctrl==false && state.super==false && state.caps==false){state.none=true;}
+            return;
+        case 170:
+            state.shift=false;
+            if(state.shift==false &&state.alt==false && state.ctrl==false && state.super==false && state.caps==false){state.none=true;}
+            return;
+    }
+    if(scancode-128 == state.last){
+        return;
+    }
+        switch(c){
+        case '%'://caps
+            state.caps=!state.caps;
+            state.none=false;
+            return;
+        case '*'://alt 
+            state.alt=true;
+            state.none=false;
+            return; 
+        case '@'://super 
+            state.super=true;
+            state.none=false;
+            return;
+        case '^'://ctrl 
+            state.ctrl=true;
+            state.none=false;
+            return;
+        case '+'://shift
+            state.shift=true;
+            state.none=false;
+            return;
+            }
+    if ((state.shift || state.caps) && !(state.caps==state.shift)){
+        c-=32;
+        keyboard_buffer[buf_position]= c;
+        buf_position = (buf_position + 1) % MAX_KEYB_BUFFER_SIZE;      
+        putc(c);
+    }else{//handling mods
+                keyboard_buffer[buf_position]= c;
+                buf_position = (buf_position + 1) % MAX_KEYB_BUFFER_SIZE;
+                putc(c);
+    }
+    state.last=scancode;
+    return;
+}
+
 void init_keyboard() {
-    i686_IRQ_RegisterHandler(1, getKey);
+    i686_IRQ_RegisterHandler(1, keyboard_irq);
     state.shift=false;
     state.ctrl=false;
     state.alt=false;
     state.super=false;
     state.caps=false;
     state.none=true;
-    
-}
-
-void getKey(Registers* regs)
-{
-    int scancode = i686_inb(0x60); // Read byte from the Keyboard data port
-    int c=chars[scancode];
-    
-    if(!state.shift){
-        switch(c){
-            case '%'://caps
-                state.caps=true;
-                state.none=false;
-                return;
-            case '*'://alt
-                state.alt=true;
-                state.none=false;
-                return; 
-            case '@'://super    
-                state.super=true;
-                state.none=false;
-                return;
-            case '^'://ctrl 
-                state.ctrl=true;
-                state.none=false;
-                return;
-            case '+'://shift
-                state.shift=true;
-                state.none=false;
-                return;
-            default:
-                keyboard_buffer[buf_position]= c;
-                buf_position = (buf_position + 1) % MAX_KEYB_BUFFER_SIZE;
-                putc(c,);
-                printf("%i",c);
-                return;
-        }
-    }else{
-
-        c-=32;
-        keyboard_buffer[buf_position]= c;
-        buf_position = (buf_position + 1) % MAX_KEYB_BUFFER_SIZE;
-        putc(c);        
-    }
-
+    state.last='\0';
 }
--- src/kernel/arch/i686/keyboard.h
+++ src/kernel/arch/i686/keyboard.h
@@ -1,6 +1,4 @@
 #pragma once
 #include "isr.h"
 
-
-void getKey(Registers* regs);
 void init_keyboard();
