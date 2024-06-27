2884369f603de77ed00a0f785e593b4737436c6f
qad7098 <qadirixx01@gmail.com>
2024-06-27 22:51:08 +0100

    got rid of the comments

--- .vscode/.gdb_script.gdb
+++
@@ -1,3 +0,0 @@
-    symbol-file /home/devil-7098/projs/gladiator_os/build/i686_debug/kernel/kernel.elf
-    set disassembly-flavor intel
-    target remote | qemu-system-i386 -S -gdb stdio -m 32 -hda /home/devil-7098/projs/gladiator_os/build/i686_debug/image.img
--- library/libwm/libwm.c
+++ library/libwm/libwm.c
@@ -1,21 +1,3 @@
-/*
- * Window manager control library
- *
- * This file is part of GLADIATOROS.
- *
- * GLADIATOROS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * GLADIATOROS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GLADIATOROS.  If not, see <https://www.gnu.org/licenses/>.
- */
 
 #include <gladiatoros.h>
 #include <stdlib.h>
